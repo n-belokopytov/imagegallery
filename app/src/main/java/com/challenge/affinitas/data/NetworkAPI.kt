37bc34e8f882603fd29ad672d4a0c19ff8793cd7
n-belokopytov <belokopytov.n@gmail.com>
2018-02-04 18:55:54 +0100

    basic setup

---
+++ app/src/main/java/com/challenge/affinitas/data/NetworkAPI.kt
@@ -0,0 +1,5 @@
+package com.challenge.affinitas.data
+
+/**
+ * Created by 805640 on 04.02.2018.
+ */
---
+++ app/src/main/java/com/challenge/affinitas/domain/features/gallery/ImageGalleryActivity.kt
@@ -0,0 +1,5 @@
+package com.challenge.affinitas.domain.features.gallery
+
+/**
+ * Created by 805640 on 04.02.2018.
+ */
